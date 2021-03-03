cf2683a248624a7e3abc3315f2f100bdb7a39bc8
prataprc <prataprc@gmail.com>
2021-03-03 13:10:21 +0530

    perf binary.

---
+++ perf.sh
@@ -0,0 +1,2 @@
+cargo run --release --bin perf --features=perf -- --loads 100000
+
---
+++ src/bin/perf.rs
@@ -0,0 +1,87 @@
+use rand::{prelude::random, rngs::SmallRng, Rng, SeedableRng};
+use structopt::StructOpt;
+
+use std::{thread, time};
+
+use cmap::Map;
+
+/// Command line options.
+#[derive(Clone, StructOpt)]
+pub struct Opt {
+    #[structopt(long = "seed")]
+    seed: Option<u128>,
+
+    #[structopt(long = "loads", default_value = "1000000")] // default 1M
+    loads: usize,
+
+    #[structopt(long = "gets", default_value = "0")] // default 1M
+    gets: usize,
+
+    #[structopt(long = "sets", default_value = "0")] // default 1M
+    sets: usize,
+
+    #[structopt(long = "rems", default_value = "0")] // default 1M
+    rems: usize,
+
+    #[structopt(long = "threads", default_value = "1")]
+    threads: usize,
+}
+
+fn main() {
+    let opts = Opt::from_args();
+    let seed = opts.seed.unwrap_or_else(random);
+    let mut rng = SmallRng::from_seed(seed.to_le_bytes());
+
+    let mut map: Map<u32, u64> = Map::new();
+
+    // initial load
+    let start = time::Instant::now();
+    for _i in 0..opts.loads {
+        let (key, val): (u32, u64) = (rng.gen(), rng.gen());
+        map.set(key, val);
+    }
+
+    println!("loaded {} items in {:?}", opts.loads, start.elapsed());
+
+    let mut handles = vec![];
+    for j in 0..opts.threads {
+        let (opts, map) = (opts.clone(), map.cloned());
+        let seed = seed + ((j as u128) * 100);
+        let h = thread::spawn(move || do_incremental(j, seed, opts, map));
+        handles.push(h);
+    }
+
+    for handle in handles.into_iter() {
+        handle.join().unwrap()
+    }
+
+    println!("{:?}", map.validate());
+}
+
+fn do_incremental(j: usize, seed: u128, opts: Opt, mut map: Map<u32, u64>) {
+    let mut rng = SmallRng::from_seed(seed.to_le_bytes());
+
+    let start = time::Instant::now();
+    let (mut sets, mut rems, mut gets) = (opts.sets, opts.rems, opts.gets);
+    while (sets + rems + gets) > 0 {
+        let key = rng.gen::<u32>();
+
+        let op = rng.gen::<usize>() % (sets + rems + gets);
+        if op < sets {
+            map.set(key, rng.gen());
+            sets -= 1;
+        } else if op < (sets + rems) {
+            map.remove(&key);
+            rems -= 1;
+        } else {
+            map.get(&key);
+            gets -= 1;
+        }
+    }
+    println!(
+        "incremental-{} for operations {}, took {:?}",
+        j,
+        opts.sets + opts.rems + opts.gets,
+        start.elapsed()
+    );
+}
