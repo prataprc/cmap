7f03a0f5f136fa8ff3bf92906c9df9606e16f87d
prataprc <prataprc@gmail.com>
2021-03-05 16:39:11 +0530

    bin/perf: cosmetics.

--- src/bin/perf.rs
+++ src/bin/perf.rs
@@ -5,6 +5,9 @@ use std::{mem, thread, time};
 
 use cmap::Map;
 
+// TODO: when we compile bin/perf with `pprof` feature and run it via valgrind
+//    there are memory-leaks. Is that normal ?
+
 type Ky = u32;
 
 /// Command line options.
