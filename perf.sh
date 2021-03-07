51cfcafe074d723c3b7818b2d95062c87af58d17
prataprc <prataprc@gmail.com>
2021-03-07 23:50:00 +0530

    check/perf: fine-tune scripts.

--- perf.sh
+++ perf.sh
@@ -1,5 +1,5 @@
 PERF=$HOME/.cargo/target/release/perf
 
-cargo run --release --bin perf --features=perf -- --loads 10000000 --gets 10000000 --sets 500000 --rems 500000
+date; cargo run --release --bin perf --features=perf -- --loads 10000000 --gets 10000000 --sets 500000 --rems 500000
 echo ".......... VALGRIND on perf ..............."
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes $PERF --loads 1000000 --gets 1000000 --sets 50000 --rems 50000 --threads 1 > valgrind.perf 2>&1
+date; valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes $PERF --loads 1000000 --gets 1000000 --sets 50000 --rems 50000 --threads 1 > valgrind.perf 2>&1
