08003b29721aa5be705ffaba729ef9858cf1666e
prataprc <prataprc@gmail.com>
2021-03-07 19:35:48 +0530

    Make and test scripts.

---
+++ Makefile
@@ -0,0 +1,2 @@
+make clean:
+	rm -f a b c d e f g h i j k l m n o p q r s t u v w x y z out.test_map out.arr_map out.dash_map
--- check.sh
+++ check.sh
@@ -26,3 +26,5 @@ for i in a b c d e f g h i j k l o p q r s t u v w x y z; do
     time cargo test --release dash_map -- --nocapture  > $i || exit $?
     echo
 done
+
+rm -f a b c d e f g h i j k l m n o p q r s t u v w x y z out.test_map out.arr_map out.dash_map
