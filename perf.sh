22826ebec916fe2298e7ff39caf09e7a90e83406
prataprc <prataprc@gmail.com>
2021-03-08 12:09:08 +0530

    perf: shortcircuit on failure.

--- perf.sh
+++ perf.sh
@@ -1,9 +1,9 @@
 PERF=$HOME/.cargo/target/release/perf
 
-date; cargo run --release --bin perf --features=perf -- --loads 10000000 --gets 10000000 --sets 500000 --rems 500000
+date; cargo run --release --bin perf --features=perf -- --loads 10000000 --gets 10000000 --sets 500000 --rems 500000 || exit $?
 echo ".......... VALGRIND on perf ..............."
-date; valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes $PERF --loads 1000000 --gets 1000000 --sets 50000 --rems 50000 --threads 1 > valgrind.perf 2>&1
+date; valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes $PERF --loads 1000000 --gets 1000000 --sets 50000 --rems 50000 --threads 1 > valgrind.perf 2>&1 || exit $?
 
-date; cargo run --release --bin perf --features=perf,compact -- --loads 10000000 --gets 10000000 --sets 500000 --rems 500000
+date; cargo run --release --bin perf --features=perf,compact -- --loads 10000000 --gets 10000000 --sets 500000 --rems 500000 || exit $?
 echo ".......... VALGRIND on perf with compact ..............."
-date; valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes $PERF --loads 1000000 --gets 1000000 --sets 50000 --rems 50000 --threads 1 > valgrind.perf 2>&1
+date; valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes $PERF --loads 1000000 --gets 1000000 --sets 50000 --rems 50000 --threads 1 > valgrind.perf 2>&1 || exit $?
