0068adc2a992c6710e7e2c02b7f02815d34fa093
prataprc <prataprc@gmail.com>
2021-03-09 16:52:31 +0530

    ci scripts: improve Makefile and check.sh

--- check.sh
+++ check.sh
@@ -6,13 +6,12 @@ exec 2>&1
 set -o xtrace
 
 exec_prg() {
-    date;
     for i in {0..10};
     do
-        time cargo +nightly test --release -- --nocapture >> check.out || exit $?
-        time cargo +nightly test -- --nocapture >> check.out || exit $?
-        time cargo +nightly test --features=compact --release -- --nocapture >> check.out || exit $?
-        time cargo +nightly test --features=compact -- --nocapture >> check.out || exit $?
+        date; time cargo +nightly test --release -- --nocapture || exit $?
+        date; time cargo +nightly test -- --nocapture || exit $?
+        date; time cargo +nightly test --features=compact --release -- --nocapture || exit $?
+        date; time cargo +nightly test --features=compact -- --nocapture || exit $?
         # repeat this for stable, once package is ready for stable.
     done
 }
