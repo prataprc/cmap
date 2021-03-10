0dae54ba7b6f43fe7fa84a64ee3427107296cdf8
prataprc <prataprc@gmail.com>
2021-03-10 17:05:42 +0530

    check-script: enable RUST_BACKTRACE and debug RUSTFLAGS
    Also add `cargo bench`.

--- check.sh
+++ check.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
 
+export RUST_BACKTRACE=full
+export RUSTFLAGS=-g
 exec > check.out
 exec 2>&1
 
@@ -12,6 +14,8 @@ exec_prg() {
         date; time cargo +nightly test -- --nocapture || exit $?
         date; time cargo +nightly test --features=compact --release -- --nocapture || exit $?
         date; time cargo +nightly test --features=compact -- --nocapture || exit $?
+        date; time cargo +nightly bench -- --nocapture || exit $?
+        date; time cargo +nightly bench --features=compact -- --nocapture || exit $?
         # repeat this for stable, once package is ready for stable.
     done
 }
