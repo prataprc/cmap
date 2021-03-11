c7fb8f8c4c0ca202af2ccef763b9c1e9d728402b
prataprc <prataprc@gmail.com>
2021-03-11 15:33:38 +0530

    ci-scripts: minor fixes.

--- Makefile
+++ Makefile
@@ -3,25 +3,17 @@
 build:
 	# ... build ...
 	cargo +nightly build
-	# cargo +stable build
 	cargo +nightly build --features=compact
-	# cargo +stable build --features=compact
 	# ... test ...
 	cargo +nightly test --no-run
-	# cargo +stable test --no-run
 	cargo +nightly test --features=compact --no-run
-	# cargo +stable test --features=compact --no-run
 	# ... bench ...
 	cargo +nightly bench --no-run
-	# cargo +stable bench --no-run
 	# ... bins ...
 	cargo +nightly build --release --bin perf --features=perf
-	# cargo +stable build --release --bin perf --features=perf
 	cargo +nightly build --release --bin perf --features=perf,compact
-	# cargo +stable build --release --bin perf --features=perf,compact
 	# ... doc ...
 	cargo +nightly doc
-	# cargo +stable doc
 	# ... meta commands ...
 	cargo +nightly clippy --all-targets --all-features
 flamegraph:
--- perf.sh
+++ perf.sh
@@ -5,8 +5,10 @@ set -o xtrace
 
 PERF=$HOME/.cargo/target/release/perf
 
-date; cargo run --release --bin perf --features=perf -- --loads 10000000 --gets 10000000 --sets 500000 --rems 500000 || exit $?
+date; cargo +nightly bench || exit $?
+
+date; cargo +nightly run --release --bin perf --features=perf -- --loads 10000000 --gets 10000000 --sets 500000 --rems 500000 || exit $?
 date; valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes $PERF --loads 1000000 --gets 1000000 --sets 50000 --rems 50000 --threads || exit $?
 
-date; cargo run --release --bin perf --features=perf,compact -- --loads 10000000 --gets 10000000 --sets 500000 --rems 500000 || exit $?
+date; cargo +nightly run --release --bin perf --features=perf,compact -- --loads 10000000 --gets 10000000 --sets 500000 --rems 500000 || exit $?
 date; valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes $PERF --loads 1000000 --gets 1000000 --sets 50000 --rems 50000 --threads || exit $?
