d942106009f411e8955a196eaeaf7f50ecc21bba
prataprc <prataprc@gmail.com>
2021-11-22 13:59:27 +0530

    make test case more vigorous.

--- src/dash_test.rs
+++ src/dash_test.rs
@@ -2,78 +2,143 @@ use arbitrary::{self, unstructured::Unstructured, Arbitrary};
 use dashmap::DashMap;
 use rand::{prelude::random, rngs::SmallRng, Rng, SeedableRng};
 
-use std::{cmp, mem, thread};
+use std::{
+    cmp, fmt, mem,
+    ops::{Add, Mul, Rem},
+    thread,
+};
 
 use super::*;
 
-type Ky = u32;
+trait Key:
+    Copy
+    + Sized
+    + Eq
+    + Add<Output = Self>
+    + Rem<Output = Self>
+    + Mul<Output = Self>
+    + fmt::Display
+    + fmt::Debug
+    + Hash
+    + Arbitrary
+{
+}
+
+impl Key for u8 {}
+impl Key for u32 {}
+impl Key for u64 {}
+impl Key for u128 {}
+
+macro_rules! test_code {
+    ($seed:expr, $keytype:ty) => {{
+        let mut rng = SmallRng::from_seed($seed.to_le_bytes());
+
+        let n_ops = [1_000, 1_000_000, 10_000_000][rng.gen::<usize>() % 3];
+        let key_max =
+            [1024 * 1024 * 1024, <$keytype>::MAX, 256, 16, 1024][rng.gen::<usize>() % 5];
+        let n_threads = {
+            let n = [1, 2, 4, 8, 16, 32, 64, 1024][rng.gen::<usize>() % 7];
+            cmp::min(key_max, n)
+        };
+        let gc_period = [0, 1, 16, 32, 256, 1024][rng.gen::<usize>() % 6];
+        let modul = key_max / (n_threads as $keytype);
+
+        println!(
+            "test_dash_map_{} seed:{} key_max:{} ops:{} threads:{} modul:{}",
+            stringify!($keytype),
+            $seed,
+            key_max,
+            n_ops,
+            n_threads,
+            modul
+        );
+
+        let mut map: Map<$keytype, u64> = {
+            let hash_builder = DefaultHasher::new();
+            Map::new(n_threads as usize + 1, hash_builder)
+        };
+        map.set_gc_period(gc_period);
+        map.print_sizing();
+        let dmap: Arc<DashMap<$keytype, u64>> = Arc::new(DashMap::new());
+
+        let mut handles = vec![];
+        for id in 0..n_threads {
+            let id = id as $keytype;
+            let seed = $seed + ((id as u128) * 100);
+
+            let (map, dmap) = (map.clone(), Arc::clone(&dmap));
+            let h =
+                thread::spawn(move || with_dashmap(id, seed, modul, n_ops, map, dmap));
+
+            handles.push(h);
+        }
+
+        for handle in handles.into_iter() {
+            handle.join().unwrap();
+        }
+
+        for item in dmap.iter() {
+            let (key, val) = item.pair();
+            assert_eq!(map.get(key), Some(*val), "for key {}", key);
+        }
+
+        println!("len {}", map.len());
+        assert_eq!(map.len(), dmap.len());
+        println!("Validate .... {:?}", map.validate());
+
+        // map.print();
+
+        mem::drop(map);
+        mem::drop(dmap);
+    }};
+}
 
 #[test]
-fn test_with_dash_map() {
+fn test_with_dash_map_u8() {
     let seed: u128 =
         [221544245499661277858524746728600114414, random()][random::<usize>() % 2];
     // let seed: u128 = 221544245499661277858524746728600114414;
-    let mut rng = SmallRng::from_seed(seed.to_le_bytes());
 
-    let key_max = [1024 * 1024 * 1024, Ky::MAX, 256, 16, 1024][rng.gen::<usize>() % 5];
-    let n_ops = [1_000, 1_000_000, 10_000_000][rng.gen::<usize>() % 3];
-    let n_threads = {
-        let n = [1, 2, 4, 8, 16, 32, 64, 1024][rng.gen::<usize>() % 7];
-        cmp::min(key_max, n)
-    };
-    let gc_period = [0, 1, 16, 32, 256, 1024][rng.gen::<usize>() % 6];
-    let modul = key_max / n_threads;
-
-    println!(
-        "test_dash_map seed:{} key_max:{} ops:{} threads:{} modul:{}",
-        seed, key_max, n_ops, n_threads, modul
-    );
-
-    let mut map: Map<Ky, u64> = {
-        let hash_builder = DefaultHasher::new();
-        Map::new(n_threads as usize + 1, hash_builder)
-    };
-    map.set_gc_period(gc_period);
-    map.print_sizing();
-    let dmap: Arc<DashMap<Ky, u64>> = Arc::new(DashMap::new());
-
-    let mut handles = vec![];
-    for id in 0..n_threads {
-        let seed = seed + ((id as u128) * 100);
-
-        let (map, dmap) = (map.clone(), Arc::clone(&dmap));
-        let h = thread::spawn(move || with_dashmap(id, seed, modul, n_ops, map, dmap));
-
-        handles.push(h);
-    }
-
-    for handle in handles.into_iter() {
-        handle.join().unwrap();
-    }
-
-    for item in dmap.iter() {
-        let (key, val) = item.pair();
-        assert_eq!(map.get(key), Some(*val), "for key {}", key);
-    }
-
-    println!("len {}", map.len());
-    assert_eq!(map.len(), dmap.len());
-    println!("Validate .... {:?}", map.validate());
-
-    // map.print();
-
-    mem::drop(map);
-    mem::drop(dmap);
+    test_code!(seed, u8);
 }
 
-fn with_dashmap(
-    id: Ky,
+#[test]
+fn test_with_dash_map_u32() {
+    let seed: u128 =
+        [221544245499661277858524746728600114414, random()][random::<usize>() % 2];
+    // let seed: u128 = 221544245499661277858524746728600114414;
+
+    test_code!(seed, u32);
+}
+
+#[test]
+fn test_with_dash_map_u64() {
+    let seed: u128 =
+        [221544245499661277858524746728600114414, random()][random::<usize>() % 2];
+    // let seed: u128 = 221544245499661277858524746728600114414;
+
+    test_code!(seed, u64);
+}
+
+#[test]
+fn test_with_dash_map_u128() {
+    let seed: u128 =
+        [221544245499661277858524746728600114414, random()][random::<usize>() % 2];
+    // let seed: u128 = 221544245499661277858524746728600114414;
+
+    test_code!(seed, u128);
+}
+
+fn with_dashmap<K>(
+    id: K,
     seed: u128,
-    modul: Ky,
+    modul: K,
     n_ops: usize,
-    mut map: Map<Ky, u64>,
-    dmap: Arc<DashMap<Ky, u64>>,
-) {
+    mut map: Map<K, u64>,
+    dmap: Arc<DashMap<K, u64>>,
+) where
+    K: Key,
+{
     let mut rng = SmallRng::from_seed(seed.to_le_bytes());
 
     let mut counts = [[0_usize; 2]; 3];
@@ -82,7 +147,7 @@ fn with_dashmap(
         let bytes = rng.gen::<[u8; 32]>();
         let mut uns = Unstructured::new(&bytes);
 
-        let mut op: Op = uns.arbitrary().unwrap();
+        let mut op: Op<K> = uns.arbitrary().unwrap();
         op = op.adjust_key(id, modul);
         // println!("{}-op -- {:?}", id, op);
         match op.clone() {
@@ -149,15 +214,21 @@ fn with_dashmap(
 }
 
 #[derive(Clone, Debug, Arbitrary)]
-enum Op {
-    Get(Ky),
-    GetWith(Ky),
-    Set(Ky, u64),
-    Remove(Ky),
+enum Op<K>
+where
+    K: Key,
+{
+    Get(K),
+    GetWith(K),
+    Set(K, u64),
+    Remove(K),
 }
 
-impl Op {
-    fn adjust_key(self, id: Ky, modul: Ky) -> Self {
+impl<K> Op<K>
+where
+    K: Key,
+{
+    fn adjust_key(self, id: K, modul: K) -> Self {
         match self {
             Op::Get(key) => {
                 let key = (id * modul) + (key % modul);
